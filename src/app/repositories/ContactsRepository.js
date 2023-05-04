8d6bdff10b42c333ae7227844868954e8600a1d1
Anderson <andersonsilvapp@gmail.com>
2023-05-04 15:57:48 -0300

    feat: show and delete contact

--- src/app/repositories/ContactsRepository.js
+++ src/app/repositories/ContactsRepository.js
@@ -1,11 +1,19 @@
 const { v4 } = require('uuid');
 
-const contacts = [
+let contacts = [
   {
     id: v4(),
     name: 'Anderson',
     email: 'andersonsilvapp@gmail.com',
     phone: '1232323232',
+    category_id: v4(),
+
+  },
+  {
+    id: v4(),
+    name: 'Manoel Gomes',
+    email: 'manoelgomes@gmail.com',
+    phone: '5698788855',
     category_id: v4(),
 
   },
@@ -15,6 +23,20 @@ class ContactsRepository {
   findAll() {
     return new Promise((resolve) => {
       resolve(contacts);
+    });
+  }
+
+  findById(id) {
+    return new Promise((resolve) => {
+      const result = contacts.find((contact) => contact.id === id);
+      resolve(result);
+    });
+  }
+
+  delete(id) {
+    return new Promise((resolve) => {
+      contacts = contacts.filter((contact) => contact.id !== id);
+      resolve();
     });
   }
 }
