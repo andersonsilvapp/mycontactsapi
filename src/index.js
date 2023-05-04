53086bd3e47ef7078661c8296a06e8e1b87f2315
Anderson <andersonsilvapp@gmail.com>
2023-05-04 15:08:26 -0300

    feat: contact controller

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 const express = require('express');
 
+const routes = require('./routes');
+
 const app = express();
+app.use(routes);
 
-app.get('/', (request, response) => {
-  response.send('Hello world!');
-});
-
-app.listen(3000, () => console.log('Sever started'));
+app.listen(3000, () => console.log('🔥Sever started at http://localhost:3000'));
