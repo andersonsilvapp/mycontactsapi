6771c71adcfee45103668c15d5a272dcfd989af4
Anderson <andersonsilvapp@gmail.com>
2023-05-04 15:38:08 -0300

    chore: change uuivv4 to v4

--- src/app/repositories/ContactsRepository.js
+++ src/app/repositories/ContactsRepository.js
@@ -1,12 +1,12 @@
-const { uuid } = require('uuidv4');
+const { v4 } = require('uuid');
 
 const contacts = [
   {
-    id: uuid(),
+    id: v4(),
     name: 'Anderson',
     email: 'andersonsilvapp@gmail.com',
     phone: '1232323232',
-    category_id: uuid(),
+    category_id: v4(),
 
   },
 ];
