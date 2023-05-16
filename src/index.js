6fc9943236aeca2171d3f80adc4fcdbbb9c116d0
Anderson <andersonsilvapp@gmail.com>
2023-05-16 16:28:55 -0300

    feat: handling api errors

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 
+require('express-async-errors');
+
 const routes = require('./routes');
 
 const app = express();
@@ -7,5 +9,10 @@ const app = express();
 app.use(express.json());
 
 app.use(routes);
+app.use((error, request, response, next) => {
+  console.log('$$$$$$$$ Error handler');
+  console.log(error);
+  response.sendStatus(500);
+});
 
 app.listen(3000, () => console.log('🔥Sever started at http://localhost:3000'));
